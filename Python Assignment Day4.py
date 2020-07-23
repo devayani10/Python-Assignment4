8259151128f08af7a3848106325bb582d54b102b
devayani10 <Devayani10022000@gmail.com>
2020-07-23 18:26:16 +0530

    Add files via upload

---
+++ Python Assignment Day4.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+a = "python programming"
+
+
+# In[2]:
+
+
+print(a)
+
+
+# In[3]:
+
+
+a[0]
+
+
+# In[4]:
+
+
+a[::]
+
+
+# In[5]:
+
+
+a[-17]
+
+
+# In[6]:
+
+
+a[0:17:1]
+
+
+# In[21]:
+
+
+b = "be strong be brave be happy"
+print(b)
+
+
+# In[13]:
+
+
+b[9:12]
+
+
+# In[14]:
+
+
+b[1:17:2]
+
+
+# In[18]:
+
+
+b[1:-17:3]
+
+
+# In[19]:
+
+
+b[::-1]
+
+
+# In[22]:
+
+
+b.count('be')
+
+
+# In[23]:
+
+
+b[0:18].count("be")
+
+
+# In[24]:
+
+
+b.find("be")
+
+
+# In[26]:
+
+
+b.rfind("be")
+
+
+# In[27]:
+
+
+str1 = "python programming"
+print(str1)
+
+
+# In[29]:
+
+
+print(str1.islower())
+
+
+# In[30]:
+
+
+print(str1.isupper())
+
+
+# In[31]:
+
+
+print(str1.upper())
+
+
+# In[32]:
+
+
+print(str1.isupper())
+
+
+# In[ ]:
+
+
+
+
